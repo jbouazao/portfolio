4de1164f621f573e4ee768791a3866e74431187f
jbouazao <jihad.bo2@gmail.com>
2022-06-20 12:56:13 +0100

    header socials target blank

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -4,7 +4,7 @@ import classes from './Socials.module.scss'
 const Socials: React.FC<{icons: {id:string, icon: string, link:string}[], link: string}> = (props) => {
 
 	return <div className = {classes.container}>
-		{props.icons.map(social => <a key = {social.id} href = {social.link}><Image src = {social.icon} alt = '' width = {20} height = {20}/></a>)}
+		{props.icons.map(social => <a key = {social.id} href = {social.link} target = '_blank' rel = 'noreferrer'><Image src = {social.icon} alt = '' width = {20} height = {20}/></a>)}
 	</div>
 }
 
